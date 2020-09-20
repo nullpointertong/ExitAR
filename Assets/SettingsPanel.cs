2401da3ddb9f228c90bc9cdfd48d1cb1a6d2b6b6
Berkan Yuksel <berkan.yuksel25@gmail.com>
2020-09-21 04:35:41 +1000

    EAR-02 Re-positioned UI components and added a settings panel.

---
+++ Assets/SettingsPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class SettingsPanel : MonoBehaviour
+{
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+
+    public void ToggleRender()
+    {
+        rend.enabled = true;
+    }
+}
