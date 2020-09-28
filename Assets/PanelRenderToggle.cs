129a24075a3a0dbeda230a2e4e902b66f4bde44b
Berkan Yuksel <berkan.yuksel25@gmail.com>
2020-09-28 12:15:14 +1000

    EAR-13 Added menu button and menu panel, attempting to toggle rendering.

---
+++ Assets/PanelRenderToggle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using UnityEngine;
+
+public class PanelRenderToggle : MonoBehaviour
+{
+    private GameObject panel;
+
+    void Start()
+    {
+        //panel = new GameObject();
+        panel.SetActive(true);
+    }
+
+    public void ToggleRender()
+    {
+        panel.SetActive(false);
+    }
+}
