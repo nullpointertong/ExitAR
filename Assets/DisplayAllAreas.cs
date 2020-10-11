1c8ed1a045f5a8df48d81e1029b63772bd9fa207
Berkan Yuksel <berkan.yuksel25@gmail.com>
2020-10-12 04:50:28 +1100

    EAR-13 Wrote logic for adding all children of Nodes into an ArrayList of GameObject type.

---
+++ Assets/DisplayAllAreas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayAllAreas : MonoBehaviour
+{
+    public GameObject floorPlan;
+    public ArrayList children;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        floorPlan = new GameObject();
+        children = new ArrayList();
+
+        for (int i = 0; i < floorPlan.transform.childCount - 1; i++)
+        {
+            GameObject tempChild = floorPlan.transform.GetChild(i).gameObject;
+
+            children.Add(tempChild);
+        }
+    }
+
+    void ShowChildren()
+    {
+        // TODO: Add logic for showing the names of all the children of the floor plan.
+    }
+}
